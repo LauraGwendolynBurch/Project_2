ea91ce6e55e1bee973a5d9bf77707595c896c617
Ilya Libershteyn <ilya.v.libershteyn@gmail.com>
2021-01-26 22:43:19 -0800

    fixed typos I had already fixed on my branch

--- db/seed.js
+++ db/seed.js
@@ -36,7 +36,7 @@ const gearSeeds = [
     itemName: "Bob's Map",
     itemDescription: "1:20,0000 Lat-Long of Mt. St. Helens",
     itemWeight: 1,
-    itemStorageLocationion: "garage",
+    itemStorageLocation: "garage",
     itemQuantityInStorage: "1",
     itemQuantityInPackingList: "0",
     UserId: 1
@@ -45,7 +45,7 @@ const gearSeeds = [
     itemName: "Bob's Backpack",
     itemDescription: "Mountain Hardwear Grotto 35L",
     itemWeight: 42,
-    itemStorageLocationion: "garage",
+    itemStorageLocation: "garage",
     itemQuantityInStorage: "1",
     itemQuantityInPackingList: "0",
     UserId: 1
@@ -54,7 +54,7 @@ const gearSeeds = [
     itemName: "John's Compass",
     itemDescription: "Cammenga Tritium Compass 3H",
     itemWeight: 4,
-    itemStorageLocationion: "garage",
+    itemStorageLocation: "garage",
     itemQuantityInStorage: "1",
     itemQuantityInPackingList: "0",
     UserId: 2
@@ -63,7 +63,7 @@ const gearSeeds = [
     itemName: "John's Map",
     itemDescription: "1:50,000 MGRS WGS84 of Mt. Rainier",
     itemWeight: 1,
-    itemStorageLocationion: "garage",
+    itemStorageLocation: "garage",
     itemQuantityInStorage: "1",
     itemQuantityInPackingList: "0",
     UserId: 2
@@ -72,7 +72,7 @@ const gearSeeds = [
     itemName: "John's Backpack",
     itemDescription: "Camp 4 28 Backpack",
     itemWeight: 42,
-    itemStorageLocationion: "garage",
+    itemStorageLocation: "garage"
   }
 ];
 
