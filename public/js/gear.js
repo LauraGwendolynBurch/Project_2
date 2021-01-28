82f4cd707ab1e3bb4557a3fd2f715c0a7dfb7ec7
Laura Gwen <lauraburch.lb@gmail.com>
2021-01-28 00:16:20 -0500

    added new post front end

--- public/js/gear.js
+++ public/js/gear.js
@@ -1,7 +1,42 @@
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
+  const itemName = $("#itemName");
+  const itemDescription = $("#itemDescription");
+  const itemWeight = $("#itemWeight");
+  const itemStorageLocation = $("#itemStorageLocation");
+  const itemQuantityInStorage = $("#itemQuantityInStorage");
+  const itemQuantityInPackingList = $("#itemQuantityInPackingList");
+
+  $("#createForm").on("submit", postFormSubmit);
+
+  function postFormSubmit(event) {
+    event.preventDefault();
+
+    if (!itemName.val().trim()) {
+      return;
+    }
+
+    const data = {
+      itemName: itemName.val(),
+      itemDescription: itemDescription.val(),
+      itemWeight: itemWeight.val(),
+      itemStorageLocation: itemStorageLocation.val(),
+      itemQuantityInStorage: itemQuantityInStorage.val(),
+      itemQuantityInPackingList: itemQuantityInPackingList.val(),
+    };
+
+    $.post("/api/gear", data).then(
+      function(gear){
+      location.reload();
+      console.log(gear);
+      }
+    )
+  }
+
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
+
+
 });
